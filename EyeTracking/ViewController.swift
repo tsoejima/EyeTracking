316dad187f498d50e7735dceff2d849ceeaf1c7c
tsoejima <t.soejimadeveloper@gmail.com>
2021-07-06 16:22:37 +0900

    EyeTracking基本部分の記述

--- EyeTracking/ViewController.swift
+++ EyeTracking/ViewController.swift
@@ -6,14 +6,45 @@
 //
 
 import UIKit
+import ARKit
 
 class ViewController: UIViewController {
-
+    let session = ARSession()
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.session.delegate = self
     }
 
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        //ARSeeeion起動時にリセット
+        reset()
+    }
+}
 
+//MARK:- ARSession
+extension ViewController: ARSessionDelegate {
+
+    func reset() {
+        guard ARFaceTrackingConfiguration.isSupported else { return }
+        let configuration = ARFaceTrackingConfiguration()
+        configuration.isLightEstimationEnabled = true
+        self.session.run(configuration, options: [.resetTracking, .removeExistingAnchors])
+    }
+
+    //MARK:- ARSessionDelegate
+    func session(_ session: ARSession, didUpdate frame: ARFrame) {
+        frame.anchors.forEach { anchor in
+            guard #available(iOS 12.0, *), let faceAnchor = anchor as? ARFaceAnchor else { return }
+
+            // FaceAnchorから左、右目の位置や向きが取得可能。
+            let left = faceAnchor.leftEyeTransform
+            print("left:\(left)")
+            let right = faceAnchor.rightEyeTransform
+            print("right:\(right)")
+        }
+    }
+
+    func session(_ session: ARSession, didFailWithError error: Error) {}
 }
 
